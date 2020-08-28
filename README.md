033e7a42c4edc7546f5743d79e5e397e1272de8b
Cláudio Henrique <00claudio.henrique@gmail.com>
2020-08-28 15:58:28 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# lyrics-search
+Website to find song lyrics
+
+Using an [API](http://docs.lyricsovh.apiary.io/) from [lyrics.ovh](https://lyrics.ovh/)
+All credits goes to [Basile Bruneau](https://github.com/NTag) ([his gh repo](https://github.com/NTag/lyrics.ovh))
---
+++ src/lyrics.js
@@ -0,0 +1,30 @@
+function findLyrics(artist, title) {
+  return fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`);
+}
+
+const form = document.querySelector('#lyrics_form');
+form.addEventListener('submit', (el) => {
+  el.preventDefault();
+  doSubmit();
+});
+
+async function doSubmit() {
+  const lyrics_el = document.querySelector('#lyrics');
+  const artist = document.querySelector('#artist');
+  const title = document.querySelector('#title');
+
+  lyrics_el.innerHTML =
+    '<div class = "spinner-grow" role = "status" > <span class = "sr-only">Loading...</span></div>';
+
+  try {
+    const lyricsResponse = await findLyrics(artist.value, title.value);
+    const data = await lyricsResponse.json();
+    if (data.lyrics) {
+      lyrics_el.innerHTML = data.lyrics;
+    } else {
+      lyrics_el.innerHTML = data.error;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
